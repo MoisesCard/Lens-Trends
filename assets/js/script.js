2eb9d1da1dada37c162bc8ae3b4d2c9bcadee13a
KingMo <macv123321@gmail.com>
2022-05-30 13:31:24 -0400

    fixed modal glitches and added some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 var introModalEl = document.querySelector('#introModal')
+var mainPageEl = document.querySelector('#main-page')
+
+var continueBtnEl = document.querySelector('#continue-btn')
+var inputBarEl = document.getElementById("#input-bar")
+
 
 window.onload = function () {
     introModalEl.classList = "display"
+    mainPageEl.classList = "noDisplay"
+    
+   
+
 }
+
+function showMainPage () {
+    introModalEl.classList = "noDisplay"
+    mainPageEl.classList = "display"
+   
+}
+
+continueBtnEl.addEventListener("click", showMainPage);
