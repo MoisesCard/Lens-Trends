c891fcdabfa0783d053d0ae579f573af084269a9
Charlotte <charlottealiciarichard@gmail.com>
2022-06-05 17:23:44 -0400

    Add Courtney's code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,93 +1,31 @@
-// PLACE HOLDER IMAGES
-
-const auth = "563492ad6f9170000100000152ffb040725b4fec8924b778c7fa6b04";
-const next = document.querySelector(".next");
-const searchbtn = document.querySelector(".searchbtn");
-const input = document.querySelector("input");
-
-let pagenr = 1;
-let search = false;
-let query = "";
-
-inputBarEl.addEventListener("input", (e) => {
-  e.preventDefault();
-  query = e.target.value;
-});
-
-async function curatedPhotos(pagenr) {
-  const data = await fetch(
-    `https://api.pexels.com/v1/curated?per_page=24&page=${pagenr}`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
-  );
-
-  const result = await data.json();
-  result.photos.forEach((photo) => {
-    const pic = document.createElement("div");
-    pic.innerHTML = `<img src=${photo.src.large} 
-        <p>Photo : ${photo.photographer}</p>
-        <a href=${photo.src.large}> Download</a>
-        
-        > </img> `;
-    document.querySelector("#placeholder-img").appendChild(pic);
-    console.log("placeholder");
-  });
-}
-
-//searched images
-async function SearchImgs(query, pagenr) {
-  const data = await fetch(
-    `https://api.pexels.com/v1/search?query=${query}&per_page=15`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    }
-  );
-
-  const result = await data.json();
-  result.photos.forEach((photo) => {
-    const pic = document.createElement("div");
-    pic.innerHTML = `<img src=${photo.src.large} 
-        <p>Photo : ${photo.photographer}</p>
-        <a href=${photo.src.large}> Download</a>
-        
-        > </img> `;
-    document.querySelector(".gallery").appendChild(pic);
-    console.log("placeholder");
-  });
-}
-
-searchbtn.addEventListener("click", () => {
-  if (input.value === "") return;
-  clear();
-  search = true;
-  SearchImgs(query, pagenr);
-});
-
-function clear() {
-  input.value = "";
-  document.querySelector(".gallery").innerHTML = "";
-  pagenr = 1;
-}
-
-curatedPhotos(pagenr);
-
-// END OF PLACEHOLDER IMAGES //
-
-//  FETCH IMAGES FROM PEXEL "ON CLICK" BUTTON
+/// //  FETCH IMAGES FROM PEXEL
 var API_KEY = "563492ad6f9170000100000152ffb040725b4fec8924b778c7fa6b04";
 var url = "https://api.pexels.com/v1/search";
-var category = "";
+// var category = "";
 
-function pullImages() {
+function showMainPage() {
+  gatherInput();
+  introModalEl.classList = "noDisplay";
+  mainPageEl.classList = "display";
+  createCatBtns();
+  startPullImages();
+}
+
+var startPullImages = function () {
+  photosContainer.innerHTML = "";
+  if (startCats.length > 0) {
+    for (var i = 0; i < startCats.length; i++) {
+      pullImages(startCats[i]);
+    }
+  } else {
+    //change to whatever the generic category is from the options
+    pullImages(general);
+  }
+};
+
+function pullImages(category) {
+  // startCats holds array of image categories from modal
+
   // ACCESS TO URL
   var queryParams = "?query=" + category;
   var finalURL = url + queryParams;
@@ -102,47 +40,93 @@ function pullImages() {
     })
     .then(function (data) {
       console.log(data);
-      // DATA.PHOTOS
-      photosContainer.innerHTML = "";
-
+      // data.photos
+      // photosContainer.innerHTML = "";
       for (let i = 0; i < data.photos.length; i++) {
         const photo = data.photos[i];
         console.log(photo);
         var imageElement = document.createElement("img");
         imageElement.src = photo.src.large;
+        imageElement.setAttribute("id", photo.id);
+        imageElement.setAttribute("name", photo.photographer);
+        imageElement.setAttribute("value", photo.alt);
         imageElement.classList.add("photo");
-        console.log(photo.src.large);
+        console.log(photo.src.original);
         photosContainer.appendChild(imageElement);
       }
     });
 }
+var cbCont = document.getElementById("category-btns-container");
+var createCatBtns = function () {
+  for (var i = 0; i < catsOptions.length; i++) {
+    var catBtn = document.createElement("button");
+    catBtn.classList = "button btn1 change-category-btn ";
+    catBtn.setAttribute("id", catsOptions[i] + "-btn");
+    catBtn.setAttribute("value", catsOptions[i]);
+    catBtn.innerHTML = catsOptions[i];
+    cbCont.appendChild(catBtn);
+  }
+};
 
-// CATEGORY BUTTONS
-var musicCatBtn = document.querySelector("#music-btn");
-musicCatBtn.addEventListener("click", setCat);
-var booksCatBtn = document.querySelector("#books-btn");
-booksCatBtn.addEventListener("click", setCat);
-var travelCatBtn = document.querySelector("#travel-btn");
-travelCatBtn.addEventListener("click", setCat);
-var sportsCatBtn = document.querySelector("#sports-btn");
-sportsCatBtn = addEventListener("click", setCat);
-var natureCatBtn = document.querySelector("#nature-btn");
-natureCatBtn = addEventListener("click", setCat);
+photosContainer.addEventListener("click", function (event) {
+  q = [event.target.id];
+  console.log(q);
+  var photoPick = document.getElementById(q);
+  console.log(photoPick);
+  console.log(photoPick.getAttribute("src"));
+  var infoURLtext = photoPick.getAttribute("src");
+  var infocreatorText = photoPick.getAttribute("name");
+  var infoAltText = photoPick.getAttribute("value");
+  var infoContainer = document.getElementById("selected-photo-info");
+  infoContainer.innerHTML = "";
+  var infoURL = document.createElement("div");
+  var infoAlt = document.createElement("div");
+  var infoCreator = document.createElement("div");
+  //add classes for styling
+  infoURL.classList = "image-info";
+  infoAlt.classList = "image-info";
+  infoCreator.classList = "image-info";
+  infoCreator.setAttribute("id", "infoURL");
+  infoURL.setAttribute("id", "infoURL");
+  infoAlt.setAttribute("id", "infoAlt");
+  infoURL.setAttribute("style", "color:white");
+  infoAlt.setAttribute("style", "color:white");
+  infoCreator.setAttribute("style", "color:white");
+  infoURL.innerHTML = "Source URL: " + infoURLtext;
+  infoAlt.innerHTML = "Alt Text: " + infoAltText;
+  infoCreator.innerHTML = "Photographer: " + infocreatorText;
+  infoContainer.append(infoURL);
+  infoContainer.append(infoAlt);
+  infoContainer.append(infoCreator);
+});
 
-var foodCatBtn = document.querySelector("#food-btn");
-foodCatBtn.addEventListener("click", setCat);
-var animalsCatBtn = document.querySelector("#animals-btn");
-animalsCatBtn.addEventListener("click", setCat);
-var plantsCatBtn = document.querySelector("#plants-btn");
-plantsCatBtn.addEventListener("click", setCat);
-var beachCatBtn = document.querySelector("#beach-btn");
-beachCatBtn.addEventListener("click", setCat);
-var danceCatBtn = document.querySelector("#dance-btn");
-danceCatBtn.addEventListener("click", setCat);
-
-var photosContainer = document.querySelector("#photos-container");
-
-function setCat(evt) {
+cbCont.addEventListener("click", function (evt) {
   category = evt.target.value;
-  pullImages();
-}
+  pullImages(category);
+});
+
+// add a clear results button
+var clearPhotos = function () {
+  photosContainer.innerHTML = "";
+};
+document
+  .getElementById("clearOutPhotos-btn")
+  .addEventListener("click", clearPhotos, searchbtn);
+
+continueBtnEl.addEventListener("click", showMainPage);
+// submitBntEl.addEventListener("submit", nameInput);
+
+// add a clear modal/ls button
+var clearStorage = function () {
+  photosContainer.innerHTML = "";
+  localStorage.clear();
+  location.reload();
+};
+document.getElementById("not-me").addEventListener("click", clearStorage);
+
+var searchbtnfunc = function () {
+  var searchInput = document.querySelector(".search-input");
+  category = searchInput.value;
+  pullImages(category);
+};
+searchbtn.addEventListener("click", searchbtnfunc);
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,86 @@ window.onload = function () {
 function showMainPage() {
   introModalEl.classList = "noDisplay";
   mainPageEl.classList = "display";
+  modalCheckboxes();
+  if (newUser) {
+  } else {
+    modalPopulate();
+  }
+}
+
+var modalCheckboxes = function () {
+  for (var c = 0; c < catsOptions.length; c++) {
+    cc = c + 1;
+    console.log(cc);
+    var inputLine = document.createElement("div");
+    var checkbox = document.createElement("input");
+    var labelCheckbox = document.createElement("label");
+    var linebreak = document.createElement("br");
+    checkbox.setAttribute("name", "preference" + cc);
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", catsOptions[c]);
+    checkbox.setAttribute("value", catsOptions[c]);
+    //or can add classes for styling it
+    // inputLine.setAttribute("style", "display:inline-flex");
+    labelCheckbox.setAttribute("for", catsOptions[c]);
+    labelCheckbox.textContent = catsOptions[c];
+
+    inputLine.appendChild(checkbox);
+    inputLine.appendChild(labelCheckbox);
+    formContainer.appendChild(inputLine);
+    formContainer.appendChild(linebreak);
+  }
+};
+
+// welcome
+
+var modalPopulate = function () {
+  // say welcome userName
+  //populate radio buttons
+  // change title
+  console.log(pastUsers);
+  document.getElementById("nameTitle").innerHTML =
+    "Welcome back " + pastUsers[0];
+  document.querySelector("#name-bar").innerHTML = "";
+  for (var a = 1; a < pastUsers.length; a++) {
+    var answerFill = document.querySelector(
+      'input[name="preference' + a + '"]'
+    );
+    console.log(answerFill);
+    if (pastUsers[a] === true) {
+      answerFill.checked = true;
+    }
+  }
+  //add a I'm not pastUsers[0] bar to clear ls and reload
+};
+
+function gatherInput() {
+  //grab this
+  if (newUser) {
+    const setName = inputBarEl.value;
+    pastUsers[0] = setName;
+  }
+  //get checkbox answers //if checked then true, else false
+
+  for (var c = 0; c < catsOptions.length; c++) {
+    cc = c + 1;
+    console.log(cc);
+    var checkedTF = document.querySelector(
+      '[name="preference' + cc + '"]'
+    ).checked;
+    console.log(checkedTF.checked);
+    if (checkedTF) {
+      startCats.push(catsOptions[c]);
+      pastUsers[cc] = true;
+      console.log(startCats);
+    } else {
+      pastUsers[cc] = false;
+    }
+  }
+
+  localStorage.setItem("pastUsers", JSON.stringify(pastUsers));
+  // console.log(setName);
+  //get categories
 }
 
 function nameInput() {
@@ -22,6 +102,96 @@ function nameInput() {
   localStorage.setItem("USER", JSON.stringify(setName));
   console.log(setName);
 }
+
+// PLACE HOLDER IMAGES
+
+const auth = "563492ad6f9170000100000152ffb040725b4fec8924b778c7fa6b04";
+const next = document.querySelector(".next");
+
+//search
+const searchbtn = document.querySelector(".searchbtn");
+const input = document.querySelector("input");
+
+let pagenr = 1;
+let search = false;
+let query = "";
+
+inputBarEl.addEventListener("input", (e) => {
+  e.preventDefault();
+  query = e.target.value;
+});
+
+async function curatedPhotos(pagenr) {
+  const data = await fetch(
+    `https://api.pexels.com/v1/curated?per_page=24&page=${pagenr}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+
+  const result = await data.json();
+  result.photos.forEach((photo) => {
+    const pic = document.createElement("div");
+    pic.innerHTML = `<img src=${photo.src.large} 
+        <p>Photo : ${photo.photographer}</p>
+        <a href=${photo.src.large}> Download</a>
+        
+        > </img> `;
+    document.querySelector("#placeholder-img").appendChild(pic);
+    console.log("placeholder");
+  });
+}
+curatedPhotos(pagenr);
+// END OF PLACEHOLDER IMAGES
+
+//SEARCHED IMAGES
+async function SearchImgs(query, pagenr) {
+  const data = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+
+  const result = await data.json();
+  result.photos.forEach((photo) => {
+    const pic = document.createElement("div");
+    pic.innerHTML = `<img src=${photo.src.large} 
+        <p>Photo : ${photo.photographer}</p>
+        <a href=${photo.src.large}> Download</a>
+        
+        > </img> `;
+    document.querySelector(".gallery").appendChild(pic);
+    console.log("placeholder");
+  });
+}
+
+searchbtn.addEventListener("click", () => {
+  if (input.value === "") return;
+  clear();
+  search = true;
+  SearchImgs(query, pagenr);
+});
+
+function clear() {
+  input.value = "";
+  document.querySelector(".gallery").innerHTML = "";
+  pagenr = 1;
+}
+// END OF SEARCHED IMAGES
+
+//  FETCH IMAGES FROM PEXEL "ON CLICK" BUTTON
+var API_KEY = "563492ad6f9170000100000152ffb040725b4fec8924b778c7fa6b04";
+var url = "https://api.pexels.com/v1/search";
+var category = "";
 
 function pullImages() {
   // ACCESS TO URL
@@ -38,19 +208,22 @@ function pullImages() {
     })
     .then(function (data) {
       console.log(data);
-      // data.photos
+      // DATA.PHOTOS
       photosContainer.innerHTML = "";
+
       for (let i = 0; i < data.photos.length; i++) {
         const photo = data.photos[i];
         console.log(photo);
         var imageElement = document.createElement("img");
         imageElement.src = photo.src.large;
         imageElement.classList.add("photo");
-        console.log(photo.src.original);
+        console.log(photo.src.large);
         photosContainer.appendChild(imageElement);
       }
     });
 }
+
+// CATEGORY BUTTONS
 var musicCatBtn = document.querySelector("#music-btn");
 musicCatBtn.addEventListener("click", setCat);
 var booksCatBtn = document.querySelector("#books-btn");
@@ -61,6 +234,18 @@ var sportsCatBtn = document.querySelector("#sports-btn");
 sportsCatBtn = addEventListener("click", setCat);
 var natureCatBtn = document.querySelector("#nature-btn");
 natureCatBtn = addEventListener("click", setCat);
+
+var foodCatBtn = document.querySelector("#food-btn");
+foodCatBtn.addEventListener("click", setCat);
+var animalsCatBtn = document.querySelector("#animals-btn");
+animalsCatBtn.addEventListener("click", setCat);
+var plantsCatBtn = document.querySelector("#plants-btn");
+plantsCatBtn.addEventListener("click", setCat);
+var beachCatBtn = document.querySelector("#beach-btn");
+beachCatBtn.addEventListener("click", setCat);
+var danceCatBtn = document.querySelector("#dance-btn");
+danceCatBtn.addEventListener("click", setCat);
+
 var photosContainer = document.querySelector("#photos-container");
 
 function setCat(evt) {
